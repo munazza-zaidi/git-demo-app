0885228d10e6be50519a99b1dd0a514f2a77b225
munazza-zaidi <jenniles@eplugins.org>
2026-02-11 15:18:50 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.title("Munazza Zaidi")
+col1, col2 = st.columns(2)
+with col1:
+    st.image('munazza.png')
+
+with col2:
+    st.write('''Munazza is a passionate and disciplined tech professional who is 
+    actively transforming her career toward Data Science and advanced automation. 
+    With 5 years of experience in manual testing and real-world plugin documentation, 
+    she combines technical curiosity with strong practical execution. 
+    She is currently sharpening her Python, Selenium, SQL, statistics, 
+    and machine learning skills while also building projects, 
+    deploying apps, and debugging real Git issues like a true developer. Alongside her 
+    technical growth, she documents her journey through her platform *“Main aur mera safar”*, 
+    inspiring others while staying committed to her own 5-month job-ready roadmap. Focused, 
+    consistent, and growth-driven, she balances personal life, professional responsibilities,
+     and continuous learning with clarity and determination.
+''')
